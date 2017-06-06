49f2968a7b313965028761253e9f13dce64b5358
Maggie Liu <mliu0805@gmail.com>
2017-06-05 22:44:34 -0400

    Add jquery script tag for ajax

---
+++ api_util.js
@@ -0,0 +1,6 @@
+exports.fetchQuotes = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+  });
+};
